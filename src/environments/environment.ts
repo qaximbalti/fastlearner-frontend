eb027c58b504a797fff62205870d709d09d2d4f5
saif <saifurrehman@vinncorp.com>
2025-08-27 17:10:35 +0500

    change properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,16 @@
-
 export const environment = {
   production: true,
   isProductionServer: false,
+
+  basePath: `https://staging.fastlearner.ai//`,
+  loginUrl: `https://staging.fastlearner.ai/:8443/auth`,
+  baseUrl: `https://staging.fastlearner.ai/:8443/api/v1/`,
+  videoUrl: `https://staging.fastlearner.ai/:8443`,
+  imageUrl: `https://staging.fastlearner.ai/:8443`,
+  applicationCourseContentUrl: `https://staging.fastlearner.ai//student/course-content/`,
+  applicationCourseDetailsUrl: `https://staging.fastlearner.ai//student/course-details/`,
+
+  // Goolge Login Credentials
+  googleClientId: '{{google-client-id}}',
+  googleSecretKey: '{{google-secret-key}}',
 };
