fb887bb96ad35200eb8859c04166d9efda2fd197
saif <saifurrehman@vinncorp.com>
2025-08-28 12:14:25 +0500

    change env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: false,
 
-  basePath: `https://staging.fastlearner.ai//`,
-  loginUrl: `https://staging.fastlearner.ai/:8443/auth`,
-  baseUrl: `https://staging.fastlearner.ai/:8443/api/v1/`,
-  videoUrl: `https://staging.fastlearner.ai/:8443`,
-  imageUrl: `https://staging.fastlearner.ai/:8443`,
-  applicationCourseContentUrl: `https://staging.fastlearner.ai//student/course-content/`,
-  applicationCourseDetailsUrl: `https://staging.fastlearner.ai//student/course-details/`,
+  basePath: `http://localhost`,
+  loginUrl: `http://localhost:8443/auth`,
+  baseUrl: `http://localhost:8443/api/v1/`,
+  videoUrl: `http://localhost:8443`,
+  imageUrl: `http://localhost:8443`,
+  applicationCourseContentUrl: `http://localhost/student/course-content/`,
+  applicationCourseDetailsUrl: `http://localhost/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
